d7770531cecadbcf656e89ab3d417f1aada8505c
ryosuke.t <bmbr.318@icloud.com>
2026-02-15 11:22:56 +0900

    fix typo: service name

--- src/main/java/io/github/ryosuke37/sylva/controller/SylvaController.java
+++ src/main/java/io/github/ryosuke37/sylva/controller/SylvaController.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class SylbaController {
+public class SylvaController {
 
     @GetMapping
     public ModelAndView top(){
         ModelAndView mav = new ModelAndView("/top");
-        mav.addObject("message", "Welcome to Sylba!");
+        mav.addObject("message", "Welcome to Sylva!");
         return mav;
     }
 }
